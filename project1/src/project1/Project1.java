78a6a34d6e4625cdb2fb06b05b3f56d419ce1980
Asus <Asus@DESKTOP-JJEVJ3K>
2018-04-12 16:27:52 +0700

    nnew

---
+++ project1/src/project1/Project1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project1;
+
+/**
+ *
+ * @author Asus
+ */
+import java.util.Scanner;
+public class Project1 {
+    public static void main (String[] args){
+System.out.println("Welcome to the airline");
+    Customer cu = new Customer();
+    cu.Customer();
+}}
+class Customer{
+    static String name2;
+    static int sale;
+    static double sale1;
+      public static void Customer(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("User Name : ");
+name2 =sc.nextLine();
+    //pcx pc = new pcx();
+pc.msxing();
+}}
